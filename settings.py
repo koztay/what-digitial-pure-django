fa902f20b9a34a81277f32ffd48df71ab8936c9d
koztay <koztay@me.com>
2021-10-18 22:49:41 +0300

    MEDIA_URL has been updated

--- settings.py
+++ settings.py
@@ -195,5 +195,6 @@ DEFAULT_STORAGE_DSN = env.str(
 DefaultStorageClass = dsn_configured_storage_class('DEFAULT_STORAGE_DSN')
 # Django's DEFAULT_FILE_STORAGE requires the class name
 DEFAULT_FILE_STORAGE = 'settings.DefaultStorageClass'
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
+MEDIA_URL = "https://whatdigitaltechnicalassessmentpuredjang-6aa810e.divio-media.com/media/"
 MEDIA_ROOT = str(ROOT_DIR.path('data/media/'))
